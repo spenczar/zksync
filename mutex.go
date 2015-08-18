2b337cb5cd6606423cf2eecef731926f2a0ab6dc
Spencer Nelson <spencer@twitch.tv>
2015-08-17 17:57:08 -0700

    Add helper methods to run funcs under locks

--- mutex.go
+++ mutex.go
@@ -446,3 +446,39 @@ func LockedExistsW(conn *zk.Conn, lock string, path string) (b bool, s *zk.Stat,
 
 	return conn.ExistsW(path)
 }
+
+// WithReadlock runs a function under a read lock on l
+func WithReadLock(l *RWMutex, f func() error) (err error) {
+	if err := l.RLock(); err != nil {
+		return err
+	}
+	defer func() {
+		unlockErr := l.Unlock()
+		if unlockErr != nil {
+			logError("failed releasing read lock on %s - err=%q", l.path, err)
+		}
+		if err == nil {
+			err = unlockErr
+		}
+	}()
+
+	return f()
+}
+
+// WithWriteLock runs a function under a write lock on l
+func WithWriteLock(l *RWMutex, f func() error) (err error) {
+	if err := l.WLock(); err != nil {
+		return err
+	}
+	defer func() {
+		unlockErr := l.Unlock()
+		if unlockErr != nil {
+			logError("failed releasing read lock on %s - err=%q", l.path, err)
+		}
+		if err == nil {
+			err = unlockErr
+		}
+	}()
+
+	return f()
+}
