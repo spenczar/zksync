cdbb1e4d890d907d40e10c6cbae2a2758f1d58bb
Spencer Nelson <spencer@justin.tv>
2015-09-01 12:34:30 -0400

    More travis privilege shenanigans

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-sudo apt-get update -qq
-sudo apt-get install -y default-jre curl
+apt-get update -qq
+apt-get install -y default-jre curl
 
 INSTALL_ROOT=/opt
 REPO="${REPO:-/vagrant}"
